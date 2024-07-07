d00ba867edbe41ea862fd3151a8c302f9af80376
Dharshini T <131947889+Sdt320@users.noreply.github.com>
2024-07-07 23:47:55 +0530

    Update Main.java

--- Static/Main.java
+++ Static/Main.java
@@ -1,1 +1,38 @@
+public class Main {
+    
+    // Static variable
+    static int count = 0;
+
+    // Static block
+    static {
+        System.out.println("Static block executed.");
+        count = 5;
+    }
+
+    // Static method
+    static void displayCount() {
+        System.out.println("Count is: " + count);
+    }
+
+    // Static nested class
+    static class NestedClass {
+        void show() {
+            // Accessing static variable from the outer class
+            System.out.println("Count from nested class is: " + count);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Accessing static method without creating an instance
+        displayCount();
+
+        // Modifying static variable
+        count += 10;
+        displayCount();
+
+        // Creating an instance of the static nested class
+        NestedClass nested = new NestedClass();
+        nested.show();
+    }
+}
 

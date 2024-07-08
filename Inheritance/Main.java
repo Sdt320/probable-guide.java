bb4d4615b2e9beeb1ab43e6be8b0e225e4f2aaaa
Dharshini T <131947889+Sdt320@users.noreply.github.com>
2024-07-08 22:01:02 +0530

    Update Main.java

--- Inheritance/Main.java
+++ Inheritance/Main.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal");
+        animal.makeSound();  // Output: Generic Animal makes a sound
 
+        Dog dog = new Dog("Rex");
+        dog.makeSound();  // Output: Rex barks
+        dog.fetch();      // Output: Rex is fetching the ball
+    }
+}

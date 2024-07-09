d1ef8f168a7bca8d37318b3abee60477cd357a94
Dharshini T <131947889+Sdt320@users.noreply.github.com>
2024-07-09 23:38:31 +0530

    Update DataStorage.java

--- ABSTRACT/DataStorage.java
+++ ABSTRACT/DataStorage.java
@@ -1,1 +1,12 @@
+public abstract class DataStorage {
+    // Abstract method to save data
+    public abstract void save(String data);
 
+    // Abstract method to load data
+    public abstract String load();
+
+    // Concrete method to log the data storage operation
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
